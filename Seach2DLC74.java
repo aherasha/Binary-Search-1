6aad6ec1acaeadd95e6c48ff0fff183c07791233
aherasha <aher.asha28@gmail.com>
2025-05-16 18:20:13 -0700

    adding sc/tc

--- Seach2DLC74.java
+++ Seach2DLC74.java
@@ -1,5 +1,7 @@
 //Did this code successfully run on Leetcode :  Yes
 //Any problem you faced while coding this :  No
+//Time complexity - O(logn)
+//Space complexity - O(1)
 public class Seach2DLC74 {
     public boolean searchMatrix(int[][] matrix, int target) {
         int left = 0, right = matrix.length * matrix[0].length-1;
